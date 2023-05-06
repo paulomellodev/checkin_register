c852d6d824fef2ddc10c358e30fe7a88dec010c7
Paulo Mello <rapha.mello87@gmail.com>
2023-05-06 13:25:20 -0300

    feat: installation of libraries and initial configuration of the server

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express, { Application, Request, Response, Router, json } from "express";
+import cors from "cors";
+
+const router = Router();
+
+router.get("/", (req: Request, res: Response) => {
+  res.send("Chegou");
+});
+
+class App {
+  public app: Application;
+
+  constructor() {
+    this.app = express();
+    this.app.use(cors());
+    this.app.use(json());
+    this.routes("/", router);
+  }
+
+  private routes(endpoint: string, routes: Router): void {
+    this.app.use(endpoint, routes);
+  }
+}
+
+export default App;
